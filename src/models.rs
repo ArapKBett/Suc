dd24ec4ec3b0d8e4bd62eb170aa12af00c8aa70a
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-08-07 15:41:23 +0300

    Create models.rs

---
+++ src/models.rs
@@ -0,0 +1,16 @@
+use chrono::{DateTime, Utc};
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+pub enum TransferType {
+    Sent,
+    Received,
+}
+
+#[derive(Clone, Serialize)]
+pub struct Transfer {
+    pub date: DateTime<Utc>,
+    pub amount: f64,
+    pub transfer_type: TransferType,
+    pub signature: String,
+}
