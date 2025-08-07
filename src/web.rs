b15cae76563a8ef4279e79fc653bf550096d5815
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-08-07 16:24:20 +0300

    Update web.rs

--- src/web.rs
+++ src/web.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, Responder};
 use serde::Serialize;
 
 use crate::models::Transfer;
@@ -8,8 +8,41 @@ struct TransferResponse {
     transfers: Vec<Transfer>,
 }
 
-pub async fn get_transfers(transfers: web::Data<Vec<Transfer>>) -> HttpResponse {
-    HttpResponse::Ok().json(TransferResponse {
-        transfers: transfers.get_ref().clone(),
-    })
+pub async fn get_transfers(transfers: web::Data<Vec<Transfer>>) -> impl Responder {
+    let html = format!(
+        r#"
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>USDC Transfers</title>
+            <style>
+                table {{ border-collapse: collapse; width: 100%; }}
+                th, td {{ border: 1px solid black; padding: 8px; text-align: left; }}
+                th {{ background-color: #f2f2f2; }}
+            </style>
+        </head>
+        <body>
+            <h1>USDC Transfers for Wallet</h1>
+            <table>
+                <tr>
+                    <th>Date</th>
+                    <th>Amount (USDC)</th>
+                    <th>Type</th>
+                    <th>Signature</th>
+                </tr>
+                {}
+            </table>
+        </body>
+        </html>
+        "#,
+        transfers
+            .iter()
+            .map(|t| format!(
+                "<tr><td>{}</td><td>{}</td><td>{:?}</td><td>{}</td></tr>",
+                t.date, t.amount, t.transfer_type, t.signature
+            ))
+            .collect::<Vec<_>>()
+            .join("")
+    );
+    HttpResponse::Ok().content_type("text/html").body(html)
 }
